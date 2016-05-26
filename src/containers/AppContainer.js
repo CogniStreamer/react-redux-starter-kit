f88b682399559fd51443600ceeaeeb104ea25fd3
Pleun Vanderbauwhede <pleun.vdb@gmail.com>
2016-05-26 15:47:03 +0100

    npm run lint:fix

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,25 +1,21 @@
-import React, { Component, PropTypes } from 'react'
-import { Router } from 'react-router'
-import { Provider } from 'react-redux'
+import React, { PropTypes } from 'react';
+import { Router } from 'react-router';
+import { Provider } from 'react-redux';
 
-class AppContainer extends Component {
-  static propTypes = {
-    history: PropTypes.object.isRequired,
-    routes: PropTypes.object.isRequired,
-    store: PropTypes.object.isRequired
-  }
-
-  render () {
-    const { history, routes, store } = this.props
-
-    return (
-      <Provider store={store}>
-        <div style={{ height: '100%' }}>
-          <Router history={history} children={routes} />
-        </div>
-      </Provider>
-    )
-  }
+function AppContainer({ history, routes, store }) {
+  return (
+    <Provider store={store}>
+      <div style={{ height: '100%' }}>
+        <Router history={history} children={routes} />
+      </div>
+    </Provider>
+  );
 }
 
-export default AppContainer
+AppContainer.propTypes = {
+  history: PropTypes.object.isRequired,
+  routes: PropTypes.object.isRequired,
+  store: PropTypes.object.isRequired
+};
+
+export default AppContainer;
